c38fe84cb46b7ce3246b812d8f8dfa450f5f7097
aukrit1993 <aukrit.1993@gmail.com>
2021-03-15 16:02:38 +0700

    ADD validate register page

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Items
+from flask import render_template, redirect, url_for, flash
+from market.models import Items, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 @app.route('/home')
@@ -13,7 +14,17 @@ def market_page():
     items = Items.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email=form.email.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err in form.errors.values():
+            flash(f'Error from create user: {err}', category='danger')
     return render_template('register.html', form=form)
